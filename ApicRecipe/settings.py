bc196208c6cabc683b368bfff78bef84d817bafd
Augusto <augusto@toteat.com>
2022-01-25 03:31:09 -0300

    cors all

--- ApicRecipe/settings.py
+++ ApicRecipe/settings.py
@@ -128,7 +128,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+CORS_ORIGIN_ALLOW_ALL = True
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
