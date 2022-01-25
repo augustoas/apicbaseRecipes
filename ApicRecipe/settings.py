bde2b99a5ec89a24973bf2ee54cb2bf85081aa19
Augusto <augusto@toteat.com>
2022-01-25 04:17:42 -0300

    try

--- ApicRecipe/settings.py
+++ ApicRecipe/settings.py
@@ -137,7 +137,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
