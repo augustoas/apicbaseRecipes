88e0489a102c0f3886444a0064047fa1956e4268
Augusto <augusto@toteat.com>
2022-01-25 04:06:16 -0300

    staticfiles

--- ApicRecipe/settings.py
+++ ApicRecipe/settings.py
@@ -127,9 +127,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_ROOT = BASE_DIR + '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
+# Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
